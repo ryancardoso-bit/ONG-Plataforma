438455ab13ef20bdda4f91e6f280586cfc381885
ryancardoso-bit <ryan.filhodasarabi@gmail.com>
2025-10-21 22:31:09 -0300

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cpf = document.getElementById("cpf");
+    const telefone = document.getElementById("telefone");
+    const cep = document.getElementById("cep");
+    const hamburger = document.querySelector(".hamburger");
+    const nav = document.querySelector("nav ul");
+
+    cpf.addEventListener("input", () => {
+        cpf.value = cpf.value.replace(/\D/g, '')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+    });
+
+    telefone.addEventListener("input", () => {
+        telefone.value = telefone.value.replace(/\D/g, '')
+            .replace(/(\d{2})(\d)/, '($1) $2')
+            .replace(/(\d{5})(\d)/, '$1-$2');
+    });
+
+    cep.addEventListener("input", () => {
+        cep.value = cep.value.replace(/\D/g, '')
+            .replace(/(\d{5})(\d)/, '$1-$2');
+    });
+
+    hamburger.addEventListener("click", () => {
+        nav.classList.toggle("show");
+    });
+});
